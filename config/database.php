47d12b84943b8a2b78ea8781b12769f0d0be545c
Kimitsu <dim.bobenko201393@yandex.ru>
2025-04-23 20:49:40 +0300

    On branch main Your branch is up to date with 'origin/main'. Changes to be committed: 	modified:   .gitignore 	modified:   composer.json 	modified:   config/database.php

--- config/database.php
+++ config/database.php
@@ -1,7 +1,15 @@
 <?php
+
+use Dotenv\Dotenv;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
 return [
-    'host' => 'db',
-    'dbname' => 'mydb',
-    'user' => 'user',
-    'password' => 'password'
+    'host' => $_ENV['DB_HOST'],
+    'dbname' => $_ENV['DB_NAME'],
+    'user' => $_ENV['DB_USER'],
+    'password' => $_ENV['DB_PASSWORD']
 ];
